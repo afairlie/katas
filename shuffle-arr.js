f9eb15f6d5bc9de9a504735ed644168e197054c8
Ariane <ariane.fairlie@gmail.com>
2020-07-17 09:30:23 -0400

    leetcode shuffle the array

---
+++ shuffle-arr.js
@@ -0,0 +1,17 @@
+// Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+// Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+var shuffle = function(nums, n) {
+  const result = [];
+  
+  for (let i in nums) {
+      result.push(nums[i])
+      result.push(nums[Number(i)+n])
+      if (i == n - 1) {
+          break;
+      }
+  }
+  return result;
+};
+
+console.log(shuffle([2,5,1,3,4,7], 3), [2,3,5,4,1,7])
