adb55d0e91f33423585d970282d899db112301f4
Ariane <ariane.fairlie@gmail.com>
2020-08-25 16:39:19 -0400

    leetcode create target array in the given order exercise

---
+++ target-array.js
@@ -0,0 +1,11 @@
+const createTargetArray = function(nums, index) {
+  const result = [];
+  
+  for (let i = 0; i < nums.length; i++) {
+      result.splice(index[i], 0, nums[i])
+  }
+  
+  return result;
+};
+
+console.log(createTargetArray([0,1,2,3,4], [0,1,2,2,1]), [0,4,1,3,2])
