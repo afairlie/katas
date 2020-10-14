549bf22be69fcb3c8e1bf4ca68c6010e1134e6aa
Ariane <ariane.fairlie@gmail.com>
2020-10-14 12:27:15 -0400

    lvl 7 string exercise, codewars

---
+++ jaden-case.py
@@ -0,0 +1,13 @@
+import string
+
+def jaden_case(s):
+  return string.capwords(s)
+
+print(jaden_case('this is a string with no caps, no longer.'))
+
+# alt solution
+
+def jaden_casing(string):
+  return ' '.join(word.capitalize() for word in string.split())
+
+print(jaden_casing('this too is a string, now capitalized'))
